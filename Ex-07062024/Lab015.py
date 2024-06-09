d214a6a2072dbcc999f17b462ee8bc28cae20598
prasanth <mpgupthan1810@gmail.com>
2024-06-09 18:19:32 +0530

    Lab014.py

---
+++ Ex-07062024/Lab015.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2)
+print(result)
+print(type(result))
+print(type(int(num1)))
+print(type(int(num2)))
+
